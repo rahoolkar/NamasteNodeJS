ac19d14ab617d69d019be5450cb6f10cf0d1f8b0
rahoolkar <batman30jan2002@gmail.com>
2026-07-14 15:24:21 +0530

    database connection

--- src/app.js
+++ src/app.js
@@ -1,22 +1,15 @@
 const express = require("express");
-
 const app = express();
 
-app.listen(3000);
+const main = require("./config/database");
 
-app.get("/user", (req, res) => {
-  res.send({ name: "rahul kar", age: 24 });
-});
-
-app.post("/user/:uid", (req, res) => {
-  res.send("Data added successfully ->"+req.params.uid);
-});
-
-app.delete("/user/:uid", (req, res) => {
-  const id = req.params.uid;
-  res.send("user deleted ->"+id);
-});
-
-app.use("/", (req, res) => {
-  res.send("meow meow ghop ghop");
-});
+main()
+  .then(() => {
+    console.log("database connected successfully");
+    app.listen(3000, () => {
+      console.log("app is listening to the port 3000");
+    });
+  })
+  .catch(() => {
+    console.error("database connection cannot be established");
+  });
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+async function main() {
+  await moongoose.connect(
+    "mongodb+srv://namastenode:aPc9v8OAmBiO44k2@cluster0.t3uxrie.mongodb.net/",
+  );
+}
+
+module.exports = main;
